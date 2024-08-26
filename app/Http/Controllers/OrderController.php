af7b8dc494ca1187344826163163e5371c5c0462
EZano <UnkNow1pro@gmail.com>
2024-08-26 14:05:28 +0330

    Order Controller and add resources to web route

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,9 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Order;
 use Illuminate\Http\Request;
 
 class OrderController extends Controller
 {
-    //
+    public function index()
+    {
+        abort_if(auth()->user() == null, 401, "شما به سیستم وارد نشده اید");
+        $orders = auth()->user()->orders;
+        //TODO: return view to show list of orders
+    }
+    public function show(Order $order)
+    {
+        $order = $order->with("items");
+        //TODO: show details of order
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,13 @@
 <?php
 
+use App\Http\Controllers\OrderController;
 use App\Http\Controllers\PageController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/index', [PageController::class,'index']);
+Route::resource('product', ProductController::class);
+Route::resource('order', OrderController::class);
